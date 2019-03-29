e45feadcba13d73989fb44d50cf7227828878e80
fachammer <fabian.achammer@gmail.com>
2019-03-29 17:33:08 +0100

    implemented better performance for bramble pasciak

--- bramble-pasciak-preconditioner.py
+++ bramble-pasciak-preconditioner.py
@@ -26,58 +26,96 @@ class ScaledPreconditioner(BaseMatrix):
     def CreateRowVector(self):
         return self.matrix.CreateRowVector()
 
+class ZeroMatrix(BaseMatrix):
+    def __init__ (self, width, height):
+        super(ZeroMatrix, self).__init__()
+        self.w = width
+        self.h = height
+    def MultAdd(self, s, x, y):
+        pass
+    def MultTransAdd(self, s, x, y):
+        pass
+    def Height (self):
+        return self.h
+    def Width (self):
+        return self.w
+    def CreateVector(self):
+        return self.CreateColVector()
+    def CreateColVector(self):
+        return Vector(self.h)
+    def CreateRowVector(self):
+        return Vector(self.w)
+
+class MatrixAB(BaseMatrix):
+    def __init__ (self, a, b):
+        super(MatrixAB, self).__init__()
+        self.a = a
+        self.b = b
+    def MultAdd(self, s, x, y):
+        y[0].data += s * self.a * x[0]
+        y[1].data += s * self.b * x[0]
+    def MultTransAdd(self, s, x, y):
+        y[0].data += s * self.a * x[0] + s * self.b.T * x[1]
+    def Height (self):
+        return self.a.height + self.b.height
+    def Width (self):
+        return self.a.width + self.b.height
+    def CreateVector(self):
+        return self.CreateColVector()
+    def CreateColVector(self):
+        return Vector(self.height)
+    def CreateRowVector(self):
+        return Vector(self.width)
+
 def BramblePasciakCG(matA, matB, matC, rhsUpper, rhsLower, preA, preM, sol=None, tol=1e-12, maxsteps=1000, printrates=True, initialize = True):
     k = 1 / min(EigenValues_Preconditioner(mat=matA, pre=preA))
     print("scale factor: ", k)
     scaledPreA = ScaledPreconditioner(k, matA, preA)
 
-    aTimesAPreMinusIdentity = matA @ scaledPreA - IdentityMatrix(matA.height)
-    matT = BlockMatrix([[aTimesAPreMinusIdentity, None], [None, IdentityMatrix(matB.height)]])
-    matN = BlockMatrix([[matA, matB.T], [matB @ aTimesAPreMinusIdentity.T, matB @ scaledPreA @ matB.T + matC if matC else matB @ scaledPreA @ matB.T]])
-    matM = matT @ matN
-    preCTimesT = BlockMatrix([[scaledPreA, None], [None, preM]])
+    matR = BlockMatrix([[matA, matB.T], [matB, matC]])
+    matS = BlockMatrix([[scaledPreA, None], [None, IdentityMatrix(matB.height)]])
+    matT = BlockMatrix([[IdentityMatrix(matA.width), None], [matB, -IdentityMatrix(matB.height)]])
+    matAB = MatrixAB(matA, matB)
+    matPTS = matAB @ matS - IdentityMatrix(matA.height + matB.height)
+    matK = matPTS @ matR
+    preSchurComplement = BlockMatrix([[IdentityMatrix(matA.width), None], [None, preM]])
+    matPreTimesPTS = preSchurComplement @ matT @ matS
+    matPreTimesK = matPreTimesPTS @ matR
 
-    tempUpper = rhsUpper.CreateVector()
-    tempLower = rhsLower.CreateVector()
-    tempUpper.data = preA * rhsUpper
-    tempLower.data = matB * tempUpper - rhsLower
-    tempUpper.data = aTimesAPreMinusIdentity * rhsUpper
-    rhs = BlockVector([tempUpper, tempLower])
-
+    rhs = BlockVector([rhsUpper, rhsLower])
     u = sol if sol else rhs.CreateVector()
+    if initialize: u[:] = 0.0
     d = rhs.CreateVector()
     p = rhs.CreateVector()
     pTemp = rhs.CreateVector()
+    s = rhs.CreateVector()
+    preValue = rhs.CreateVector()
 
-    if initialize: u[:] = 0.0
-
-    d.data = rhs - matM * u
-    sUpper = rhsUpper.CreateVector()
-    sLower = tempLower.CreateVector()
-    sUpper.data = rhsUpper
-    sLower.data = tempLower
-    s = BlockVector([sUpper, sLower])
-    s.data += -matN * u
-    p.data = preCTimesT * s
+    pTemp.data = rhs - matR * u
+    s.data = matS * pTemp
+    d.data = matAB * s - rhs + matR * u
+    p.data = preSchurComplement @ matT * s
     pTemp.data = p
 
     pdn = InnerProduct(p, d)
     err0 = sqrt(abs(pdn))
     print("error 0: ", err0)
 
-    if pdn==0:
+    if pdn == 0:
         return u
 
     for it in range(maxsteps):
-        p.data = matM * pTemp
+        p.data = -matR * pTemp
+        preValue.data = -matS * p
+        p.data += matAB * preValue
         pd = pdn
         as_s = InnerProduct(pTemp, p)
         alpha = pd / as_s
         u.data += alpha * pTemp
-        s.data += (-alpha) * matN * pTemp
         d.data += (-alpha) * p
+        s.data += (-alpha) * preValue
 
-        p.data = preCTimesT * s
+        p.data = preSchurComplement @ matT * s
 
         pdn = InnerProduct(p, d)
         beta = pdn / pd
@@ -120,7 +158,7 @@ p,q = Q.TnT()
 a = BilinearForm(V)
 a += SymbolicBFI(InnerProduct(grad(u),grad(v)))
 
-preA = Preconditioner(a, 'multigrid')
+preA = Preconditioner(a, 'local')
 
 b = BilinearForm(trialspace=V, testspace=Q)
 b += SymbolicBFI(div(u)*q)
@@ -145,24 +183,26 @@ gfp = GridFunction(Q, name="p")
 uin = CoefficientFunction( (1.5*4*y*(0.41-y)/(0.41*0.41), 0) )
 gfu.Set(uin, definedon=mesh.Boundaries("inlet"))
 sol = BlockVector( [gfu.vec, gfp.vec] )
-bramblePasciakTimer = Timer("BramblePasciakCG")
-bramblePasciakTimer.Start()
-BramblePasciakCG(a.mat, b.mat, None, f.vec, g.vec, preA, preM, sol, initialize=False, tol=1e-7, maxsteps=1000)
-bramblePasciakTimer.Stop()
 
-gfu = GridFunction(V, name="u")
-gfp = GridFunction(Q, name="p")
-uin = CoefficientFunction( (1.5*4*y*(0.41-y)/(0.41*0.41), 0) )
-gfu.Set(uin, definedon=mesh.Boundaries("inlet"))
-sol = BlockVector( [gfu.vec, gfp.vec] )
-K = BlockMatrix( [ [a.mat, b.mat.T], [b.mat, None] ] )
-C = BlockMatrix( [ [preA, None], [None, preM] ] )
-rhs = BlockVector ( [f.vec, g.vec] )
-sol = BlockVector( [gfu.vec, gfp.vec] )
-minResTimer = Timer("MinRes")
-minResTimer.Start()
-solvers.MinRes(mat=K, pre=C, rhs=rhs, sol=sol, initialize=False, tol=1e-7, maxsteps=1000)
-minResTimer.Stop()
+with TaskManager(pajetrace=100*1000*1000):
+    bramblePasciakTimer = Timer("BramblePasciakCG")
+    bramblePasciakTimer.Start()
+    BramblePasciakCG(a.mat, b.mat, None, f.vec, g.vec, preA, preM, sol, initialize=False, tol=1e-7, maxsteps=1000)
+    bramblePasciakTimer.Stop()
+
+    gfu = GridFunction(V, name="u")
+    gfp = GridFunction(Q, name="p")
+    uin = CoefficientFunction( (1.5*4*y*(0.41-y)/(0.41*0.41), 0) )
+    gfu.Set(uin, definedon=mesh.Boundaries("inlet"))
+    sol = BlockVector( [gfu.vec, gfp.vec] )
+    K = BlockMatrix( [ [a.mat, b.mat.T], [b.mat, None] ] )
+    C = BlockMatrix( [ [preA, None], [None, preM] ] )
+    rhs = BlockVector ( [f.vec, g.vec] )
+    sol = BlockVector( [gfu.vec, gfp.vec] )
+    minResTimer = Timer("MinRes")
+    minResTimer.Start()
+    solvers.MinRes(mat=K, pre=C, rhs=rhs, sol=sol, initialize=False, tol=1e-7, maxsteps=1000)
+    minResTimer.Stop()
 
 XV = VectorH1(mesh, order=2, dirichlet="wall|inlet|cyl")
 V.SetOrder(TRIG,3)
@@ -184,9 +224,8 @@ res.data = -a.mat * gfu.vec
 inv = a.mat.Inverse(freedofs=X.FreeDofs(), inverse="umfpack")
 gfu.vec.data += inv * res
 directTimer.Stop()
+Draw(Norm(gfu.components[0]), mesh, "vel")
 
 print("BramblePasciakCG took", bramblePasciakTimer.time, "seconds")
 print("MinRes took", minResTimer.time, "seconds")
 print("direct solver took", directTimer.time, "seconds")
-
-Draw(Norm(gfu.components[0]), mesh, "vel")
