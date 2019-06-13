086ca255d654b7130419cd240e098d70ba9b3188
fachammer <fabian.achammer@gmail.com>
2019-06-13 17:29:39 +0200

    implemented stokes mcs with bramble pasciak cg

--- stokes_hcurldiv.py
+++ stokes_hcurldiv.py
@@ -3,6 +3,7 @@ from ngsolve import *
 from netgen.geom2d import SplineGeometry
 #from minres import MinRes
 from ngsolve.solvers import MinRes
+from bramble_pasciak_cg import bramble_pasciak_cg
 
 order = 2
 
@@ -20,10 +21,8 @@ VHat = VectorFacet(mesh, order=order - 1, dirichlet="wall|inlet|cyl")
 Q = L2(mesh, order=order - 1)
 Sigma.SetCouplingType(IntRange(0, Sigma.ndof), COUPLING_TYPE.HIDDEN_DOF)
 Sigma = Compress(Sigma)
-Q.SetCouplingType(IntRange(0, mesh.ne), COUPLING_TYPE.WIREBASKET_DOF)
-Q.FinalizeUpdate()
-X = FESpace([V1, Q, VHat, Sigma])
-(u, p, u_hat, sigma), (v, q, v_hat, tau) = X.TnT()
+X = FESpace([V1, VHat, Sigma])
+(u, u_hat, sigma), (v, v_hat, tau) = X.TnT()
 
 
 n = specialcf.normal(mesh.dim)
@@ -40,30 +39,50 @@ a += -(sigma * n) * n * (v * n) * dx(element_boundary=True)
 a += -(tau * n) * n * (u * n) * dx(element_boundary=True)
 a += -(tau * n) * tang(u_hat) * dx(element_boundary=True)
 a += -(sigma * n) * tang(v_hat) * dx(element_boundary=True)
-a += div(u) * q * dx + div(v) * p * dx
-a += 1e-8 * p * q * dx
 
-preA = Preconditioner(a, 'local')
+p, q = Q.TnT()
+
+b = BilinearForm(trialspace=X, testspace=Q)
+b += div(u) * q * dx
+
+preA = Preconditioner(a, 'bddc')
+
+m = BilinearForm(Q)
+m += p * q * dx
+
+preM = Preconditioner(m, 'local')
+
 
 a.Assemble()
+b.Assemble()
+m.Assemble()
 
 f = LinearForm(X)
 f += SymbolicLFI(CoefficientFunction((0, x - 0.5)) * v)
 f.Assemble()
 
+g = LinearForm(Q)
+g.Assemble()
+
 grid_function = GridFunction(X)
+grid_function_pressure = GridFunction(Q)
 uin = CoefficientFunction((1.5 * 4 * y * (0.41 - y) / (0.41 * 0.41), 0))
 grid_function.components[0].Set(uin, definedon=mesh.Boundaries("inlet"))
 
 Draw(grid_function.components[0])
-Draw(grid_function.components[2])
+Draw(grid_function_pressure)
 #Draw(x - 0.5, mesh, "source")
 
-res = grid_function.vec.CreateVector()
-res.data = f.vec - a.mat * grid_function.vec
-res_2 = res.CreateVector()
-MinRes(mat=a.mat, pre=preA, rhs=f.vec, sol=grid_function.vec, initialize=False,
-       tol=1e-7, maxsteps=100000)
+solution = BlockVector([grid_function.vec, grid_function_pressure.vec])
+bramble_pasciak_cg(a.mat, b.mat, None, preA, preM,
+                   f.vec, g.vec, solution=solution)
+
+
+#res = grid_function.vec.CreateVector()
+#res.data = f.vec - a.mat * grid_function.vec
+#res_2 = res.CreateVector()
+# MinRes(mat=a.mat, pre=preA, rhs=f.vec, sol=grid_function.vec, initialize=False,
+#       tol=1e-7, maxsteps=100000)
 #grid_function.vec.data += res_2
 
 #res = grid_function.vec.CreateVector()
