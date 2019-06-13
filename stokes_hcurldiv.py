6c81089a70ca319e3a0cbe77353777870100e4ee
fachammer <fabian.achammer@gmail.com>
2019-06-13 11:03:39 +0200

    implemented working minres solver

--- stokes_hcurldiv.py
+++ stokes_hcurldiv.py
@@ -19,9 +19,11 @@ Sigma = HCurlDiv(mesh, order=order - 1, orderinner=order, discontinuous=True)
 VHat = VectorFacet(mesh, order=order - 1, dirichlet="wall|inlet|cyl")
 Q = L2(mesh, order=order - 1)
 Sigma.SetCouplingType(IntRange(0, Sigma.ndof), COUPLING_TYPE.HIDDEN_DOF)
-#Sigma = Compress(Sigma)
-X = FESpace([V1, Sigma, Q, VHat])
-(u, sigma, p, u_hat), (v, tau, q, v_hat) = X.TnT()
+Sigma = Compress(Sigma)
+Q.SetCouplingType(IntRange(0, mesh.ne), COUPLING_TYPE.WIREBASKET_DOF)
+Q.FinalizeUpdate()
+X = FESpace([V1, Q, VHat, Sigma])
+(u, p, u_hat, sigma), (v, q, v_hat, tau) = X.TnT()
 
 
 n = specialcf.normal(mesh.dim)
@@ -31,7 +33,7 @@ def tang(vec):
     return vec - (vec * n) * n
 
 
-a = BilinearForm(X)
+a = BilinearForm(X, eliminate_hidden=True)
 a += -InnerProduct(sigma, tau) * dx
 a += div(sigma) * v * dx + div(tau) * u * dx
 a += -(sigma * n) * n * (v * n) * dx(element_boundary=True)
@@ -39,9 +41,9 @@ a += -(tau * n) * n * (u * n) * dx(element_boundary=True)
 a += -(tau * n) * tang(u_hat) * dx(element_boundary=True)
 a += -(sigma * n) * tang(v_hat) * dx(element_boundary=True)
 a += div(u) * q * dx + div(v) * p * dx
-a += -1e-8 * p * q * dx
+a += 1e-8 * p * q * dx
 
-#preA = Preconditioner(a, 'direct')
+preA = Preconditioner(a, 'local')
 
 a.Assemble()
 
@@ -57,16 +59,16 @@ Draw(grid_function.components[0])
 Draw(grid_function.components[2])
 #Draw(x - 0.5, mesh, "source")
 
-#res = grid_function.vec.CreateVector()
-#res.data = f.vec - a.mat * grid_function.vec
-#res_2 = res.CreateVector()
-# MinRes(mat=a.mat, pre=preA, rhs=f.vec, sol=grid_function.vec, initialize=False,
-#       tol=1e-7, maxsteps=100000)
-#grid_function.vec.data += res_2
-
 res = grid_function.vec.CreateVector()
 res.data = f.vec - a.mat * grid_function.vec
-inv = a.mat.Inverse(freedofs=X.FreeDofs(), inverse="umfpack")
-grid_function.vec.data += inv * res
+res_2 = res.CreateVector()
+MinRes(mat=a.mat, pre=preA, rhs=f.vec, sol=grid_function.vec, initialize=False,
+       tol=1e-7, maxsteps=100000)
+#grid_function.vec.data += res_2
+
+#res = grid_function.vec.CreateVector()
+#res.data = f.vec - a.mat * grid_function.vec
+#inv = a.mat.Inverse(freedofs=X.FreeDofs(), inverse="umfpack")
+#grid_function.vec.data += inv * res
 Redraw()
 input("press enter to stop")
