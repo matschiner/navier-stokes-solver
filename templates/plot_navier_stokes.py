971e7e62e8a4090ebf8a5e600d52ac364e852c3f
fachammer <fabian.achammer@gmail.com>
2019-06-29 14:41:27 +0200

    added proper order and mesh size gauss seidel comparison plots

--- templates/plot_navier_stokes.py
+++ templates/plot_navier_stokes.py
@@ -13,7 +13,7 @@ def plot_over_order(value, gauss_seidel=True):
     data.groupby('gauss_seidel_enabled')\
         .get_group(gauss_seidel)\
         .pivot(index='order', columns='mesh_size', values=value)\
-        .plot.bar(title=f"{value}, GS={gauss_seidel}")
+        .plot.bar(title=f"{value}, GS={gauss_seidel}").legend(loc=4)
 
 def plot_gs_comparison_over_mesh_size(order, value):
     data = pd.read_csv("data.csv", index_col=0)
@@ -35,10 +35,10 @@ plot_over_mesh_size('time')
 plot_over_order('time')
 plot_over_mesh_size('iterations')
 plot_over_order('iterations')
-plot_gs_comparison_over_mesh_size(order=2, value='iterations')
-plot_gs_comparison_over_order(mesh_size=1/2, value='iterations')
-plot_gs_comparison_over_mesh_size(order=2, value='time')
-plot_gs_comparison_over_order(mesh_size=1/2, value='time')
+plot_gs_comparison_over_mesh_size(order=4, value='iterations')
+plot_gs_comparison_over_order(mesh_size=1/8, value='iterations')
+plot_gs_comparison_over_mesh_size(order=4, value='time')
+plot_gs_comparison_over_order(mesh_size=1/8, value='time')
 plt.show()
 #data = pd.read_csv("templates/data.csv", index_col=0)
 #data.pivot(index='mesh_size', columns='order', values='iterations').plot(style='-o', logx=True, ylim=(0, 200))
