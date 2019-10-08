7a65002db1cb9a19ac19c3e0b14508e53523fb26
Max Matschiner <max@bymm.at>
2019-10-08 21:36:06 +0200

    embedding and basistrafo added

---
+++ embedding/basistrafo.py
@@ -0,0 +1,64 @@
+from ngsolve import *
+from netgen.geom2d import unit_square
+
+mesh = Mesh(unit_square.GenerateMesh(maxh=0.2))
+
+fesh1 = VectorH1(mesh, order=2)
+feshdiv = HDiv(mesh, order=2)
+fesvec = VectorFacet(mesh, order=2)
+fescomp = FESpace([feshdiv, fesvec])
+
+gfuh1 = GridFunction(fesh1)
+gfuh1.Set((x * x, y * y))
+
+amixed = BilinearForm(trialspace=fesh1, testspace=fescomp)
+acomp = BilinearForm(fescomp)
+
+(u, uf), (v, vf) = fescomp.TnT()
+vdual = v.Operator("dual")
+uh1 = fesh1.TrialFunction()
+
+n = specialcf.normal(mesh.dim)
+
+
+def tang(v): return v - (v * n) * n
+
+
+vf = tang(vf)
+
+dS = dx(element_boundary=True)
+acomp += u * vdual * dx
+acomp += u * vdual * dS
+acomp += uf * vf * dS
+acomp.Assemble()
+
+amixed += uh1 * vdual * dx
+amixed += uh1 * vdual * dS
+amixed += uh1 * vf * dS
+amixed.Assemble()
+
+print("ahdiv =", acomp.mat)
+print("amixed =", amixed.mat)
+
+gfucomp = GridFunction(fescomp, name="ucomp")
+# transform = ahdiv.mat.Inverse() @ amixed.mat
+# gfuhdiv.vec.data = transform * gfuh1.vec
+
+eblocks = []
+for e in mesh.edges:
+    eblocks.append(fescomp.GetDofNrs(e))
+fblocks = []
+for f in mesh.faces:
+    fblocks.append(fescomp.GetDofNrs(f))
+
+print(eblocks)
+print(fblocks)
+
+einv = acomp.mat.CreateBlockSmoother(eblocks)
+finv = acomp.mat.CreateBlockSmoother(fblocks)
+
+transform = (einv + finv @ (IdentityMatrix() - acomp.mat @ einv)) @ amixed.mat
+gfucomp.vec.data = transform * gfuh1.vec
+
+Draw(gfuh1)
+Draw(gfucomp.components[0], mesh, "uhdiv")
---
+++ embedding/embedding_simple.py
@@ -0,0 +1,121 @@
+from ngsolve import *
+from ngsolve.internal import visoptions
+from ngsolve.la import EigenValues_Preconditioner
+from netgen.geom2d import SplineGeometry, unit_square
+
+import netgen.gui
+
+
+def tang(v): return v - (v * n) * n
+
+
+mesh = Mesh(unit_square.GenerateMesh(maxh=0.1))
+
+nu = 1
+
+zeta = x ** 2 * (1 - x) ** 2 * y ** 2 * (1 - y) ** 2
+u_ex = CoefficientFunction((zeta.Diff(y), -zeta.Diff(x)))
+
+f_1 = -nu * (u_ex[0].Diff(x).Diff(x) + u_ex[0].Diff(y).Diff(y))  # + p_ex.Diff(x)
+f_2 = -nu * (u_ex[1].Diff(x).Diff(x) + u_ex[1].Diff(y).Diff(y))  # + p_ex.Diff(y)
+force = CoefficientFunction((x, x))
+
+order = 2
+
+V = HDiv(mesh, order=order, dirichlet="right|bottom|left|top")
+Vhat = TangentialFacetFESpace(mesh, order=order, dirichlet="right|bottom|left|top")
+
+n = specialcf.normal(mesh.dim)
+h = specialcf.mesh_size
+
+
+def tang(vec):
+    return vec - (vec * n) * n
+
+
+dS = dx(element_boundary=True)
+dS2 = dx(element_boundary=True)
+dSS = dx(element_vb=BBND)
+
+X = FESpace([V, Vhat])
+(u, uhat), (v, vhat) = X.TnT()
+VH1 = VectorH1(mesh, order=order, dirichlet="right|bottom|left|top", )
+vH1trial, vH1test = VH1.TnT()
+
+v_dual = v.Operator("dual")
+
+generic_H1_func = GridFunction(VH1)
+generic_H1_func_transformed = GridFunction(X)
+generic_H1_func.Set((x * x, y * y))
+
+M = BilinearForm(trialspace=VH1, testspace=X)
+M += vH1trial * v_dual * dx
+M += vH1trial * v_dual * dS
+M += vH1trial * tang(vhat) * dS
+M.Assemble()
+
+Mw = BilinearForm(X, X)
+Mw += u * v_dual * dx
+Mw += u * v_dual * dS
+Mw += uhat * tang(vhat) * dS
+Mw.Assemble()
+
+Mw_inverse = Mw.mat.Inverse(inverse="umfpack")
+E = Mw_inverse @ M.mat
+
+# , freedofs=BitArray([True for k in range(V.ndof)] + [False for k in range(Vhat.ndof)]))
+# tmp = M.mat.CreateColVector()
+# tmp.data = M.mat * generic_H1_func.vec
+
+# blocks = [list(X.GetDofNrs(f)) for f in mesh.facets] \
+#         + [list(X.GetDofNrs(m)) for m in mesh.Elements()]
+
+# blockjac = Mw.mat.CreateBlockSmoother(blocks)
+
+# blockjac.Smooth(generic_H1_func_transformed.vec, tmp)
+# generic_H1_func_transformed.vec.data = Mw_inverse* tmp
+generic_H1_func_transformed.vec.data = E * generic_H1_func.vec
+
+laplaceH1 = BilinearForm(VH1)
+laplaceH1 += InnerProduct(grad(vH1trial), grad(vH1test)) * dx
+laplaceH1.Assemble()
+
+laplaceH1_inverse = laplaceH1.mat.Inverse(freedofs=VH1.FreeDofs())
+
+Ahat_inv = E @ laplaceH1_inverse @ E.T
+
+gradu = CoefficientFunction((grad(u),), dims=(2, 2))
+gradv = CoefficientFunction((grad(v),), dims=(2, 2))
+
+alpha = 10
+
+a = BilinearForm(X, condense=True)
+a += SymbolicBFI(nu * InnerProduct(gradu, gradv))
+a += SymbolicBFI(nu * InnerProduct(gradu.trans * n, tang(vhat - v)), element_boundary=True)
+a += SymbolicBFI(nu * InnerProduct(gradv.trans * n, tang(uhat - u)), element_boundary=True)
+a += SymbolicBFI(nu * alpha * (order + 1) ** 2 / h * InnerProduct(tang(vhat - v), tang(uhat - u)), element_boundary=True)
+
+a.Assemble()
+
+# preA = c.mat
+preA = Ahat_inv # jacobi part still missing
+
+f = LinearForm(X)
+f += SymbolicLFI(force * v)
+f.Assemble()
+
+gfu = GridFunction(X)
+
+if a.condense:
+    f.vec.data += a.harmonic_extension_trans * f.vec
+    solvers.CG(mat=a.mat, pre=preA, rhs=f.vec, sol=gfu.vec, maxsteps=40)
+    gfu.vec.data += a.inner_solve * f.vec
+    gfu.vec.data += a.harmonic_extension * gfu.vec
+else:
+    gfu.vec.data = a.mat.Inverse(X.FreeDofs(True), inverse="sparsecholesky") * f.vec
+
+vel = CoefficientFunction((gfu.components[0]))
+
+Draw(vel, mesh, "vel")
+visoptions.scalfunction = 'vel:0'
+input("end")
